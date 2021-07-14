69926fe8b3ac7a8d212446b455360fa6edf33205
Michael Barron <michael.barron08@gmail.com>
2021-07-13 22:03:49 -0500

    final commit

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, session, redirect
+app = Flask(__name__)
+app.secret_key= 'akfnmbvdbmdgbp'
+
+
+
+@app.route('/')
+def counter():
+    
+    if 'count' not in session:
+        session['count'] = 0
+    else:
+        session['count'] += 1
+    
+    
+    
+    
+    return render_template('index.html', count = session ['count'])
+
+
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()
+
+    return redirect('/')
+
+
+
+
+
+
+if __name__=="__main__": 
+    app.run(debug=True)
+
